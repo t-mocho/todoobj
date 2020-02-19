500cf5521f1d71d0d4a408f3b0e9bc303aab582d
t-mocho <55965167+t-mocho@users.noreply.github.com>
2020-02-19 11:43:09 +0900

    ボタン押下li追加

--- app.js
+++ app.js
@@ -11,10 +11,30 @@ const addbtn = document.getElementById('addbtn');
 addbtn.onclick = function() {
   const text = document.getElementById('addtext').value;
   const todo = {};
-  // 各種値を更新
+  // 値更新
   todo.task = text;
   todo.done = false;
   // 配列追加
   todos.push(todo);
+  // 書き出し'
+  addtodo(todos);
+  // 入力値リセット
+  let re = document.getElementById('addtext');
+  re.value = "";
+};
+
+// 書き出し関数
+function addtodo(todos) {
+  // ul取得
+  const tasklist = document.getElementById('tasklist');
+  // li作成
+  var newli = document.createElement("li");
+  // todosの配列を1つずつとる
+  for (let i in todos) {
+    var str = todos[i].task;
+  }
+  // HTMLに書き出し
+  newli.appendChild(document.createTextNode(str));
+  tasklist.appendChild(newli);
   console.log(todos);
 };
