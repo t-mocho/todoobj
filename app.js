ff3f2fbc41a79dc38b0f1b88b12fda8de33b732d
t-mocho <55965167+t-mocho@users.noreply.github.com>
2020-02-20 11:56:09 +0900

    削除がおしい・・

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 // todolist
 var todos = [{
+  num: 0,
   task: addtext,
   done: false
 }];
 // 0番目削除
 todos.shift();
+
+// id
+const tasklist = document.getElementById('tasklist');
 
 // 追加
 const addbtn = document.getElementById('addbtn');
@@ -27,32 +31,52 @@ addbtn.onclick = function() {
 
 // 書き出し関数
 function addtodo(todos) {
-  // ul取得
-  const tasklist = document.getElementById('tasklist');
   // li作成
   var newli = document.createElement('li');
   // todosの配列を1つずつとる
   for (let i in todos) {
     var addlist = todos[i].task;
   }
+  // num更新
+  for (var i = 0; i < todos.length; i++) {
+    todos[i].num = i;
+  }
   // HTMLに書き出し
-  newli.insertAdjacentHTML('afterbegin', '<i class="material-icons">favorite</i>');
+  newli.insertAdjacentHTML('afterbegin', '<i class="material-icons add">favorite</i>');
   newli.appendChild(document.createTextNode(addlist));
+  newli.insertAdjacentHTML('beforeend', '<i class="material-icons del">delete</i>');
   tasklist.appendChild(newli);
 };
 
-// 完了タスク
-const tasklist = document.getElementById('tasklist');
+// 完了
 tasklist.addEventListener('click', (e) => {
   let target = e.target;
-  // iのみ処理を行う
-  const icon = document.getElementsByClassName('material-icons');
-  for (var i = 0; i < icon.length; i++) {
-    if (target === icon[i]) {
-      target.classList.toggle('end');
-      // done変更
-      todos[i].done = !todos[i].done
+  // addclass
+  const icon = document.getElementsByClassName('material-icons add');
+  if (target.classList.contains('add')) {
+    for (var i = 0; i < todos.length; i++) {
+      // ハートクリックでtoggle
+      if (target === icon[i]) {
+        target.classList.toggle('end');
+        // done変更
+        todos[i].done = !todos[i].done;
+      }
     }
   }
-  console.log(todos);
+});
+
+// 削除
+tasklist.addEventListener('click', (e) => {
+  let target = e.target;
+  // delclass
+  const delicon = document.getElementsByClassName('material-icons del');
+  // todosの長さからiの位置を指定
+  for (var i = 0; i < todos.length; i++) {
+    // HTML削除
+    if (target === delicon[i]) {
+      todos.splice(todos[i].num, 1);
+      target.parentElement.remove();
+      console.log(todos);
+    }
+  }
 });
